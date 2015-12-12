4524bd4a936710a01f24d5daf112c731d3bd4531
Narya92 <a.memis@ostfalia.de>
2015-12-12 23:33:22 +0100

    Kamera aufrufen!

--- app/src/main/java/com/mobileanwendungen/cityhunt/Detail.java
+++ app/src/main/java/com/mobileanwendungen/cityhunt/Detail.java
@@ -1,7 +1,12 @@
 package com.mobileanwendungen.cityhunt;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Detail extends AppCompatActivity {
 
@@ -11,5 +16,15 @@ public class Detail extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         String title = getIntent().getStringExtra("title");
         setTitle(title);
+
+        ImageButton kameraButton = (ImageButton) findViewById(R.id.kameraButton);
+
+        kameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent itent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(itent,0);
+            }
+        });
     }
 }
