373b8423bfa6eb67b840f508109460873d001f25
Narya92 <a.memis@ostfalia.de>
2015-12-12 18:13:11 +0100

    splash Screen changed to mapsActivity

--- app/src/main/java/com/mobileanwendungen/cityhunt/SplashScreen.java
+++ app/src/main/java/com/mobileanwendungen/cityhunt/SplashScreen.java
@@ -1,7 +1,12 @@
 package com.mobileanwendungen.cityhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.os.Handler;
+
+import com.mobileanwendungen.cityhunt.gui.MapsActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+     Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent startMapsActivity = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(startMapsActivity);
+
+
+            }
+        },2000
+        );
+
+
     }
 }
