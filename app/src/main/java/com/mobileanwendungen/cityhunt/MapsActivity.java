c5c544b528228ee090c72111958fb1360b4cbd6c
thorben.wilde <thorben.wilde@gmx.de>
2015-12-30 15:04:26 +0100

    Splash-Screen ist nun vollbild Map ist nun smarter

--- app/src/main/java/com/mobileanwendungen/cityhunt/MapsActivity.java
+++ app/src/main/java/com/mobileanwendungen/cityhunt/MapsActivity.java
@@ -2,16 +2,22 @@ package com.mobileanwendungen.cityhunt;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.mobileanwendungen.cityhunt.model.Sight;
 import com.mobileanwendungen.cityhunt.model.SightList;
@@ -22,9 +28,17 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMyLocationChangeListener, GoogleMap.OnMapClickListener {
+
+    Map<Marker, Sight> markerSightMap = new HashMap<>();
+    Marker activatedMarker = null;
+    Marker ownLocationMarker = null;
+    GoogleMap googleMaps = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,11 +74,34 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap map) {
 
+        googleMaps = map;
+
         for(Sight sight : AppDataExchange.listOfSights){
-            map.addMarker(new MarkerOptions()
+
+            MarkerOptions markerOps = new MarkerOptions()
                     .position(new LatLng(sight.getLatitude(), sight.getLongitude()))
-                    .title(sight.getName()));
+                    .title(sight.getName());
+
+            switch(sight.getType()){
+                case MONUMENT:
+                    markerOps.icon(BitmapDescriptorFactory.fromResource(R.drawable.logo_denkmal));
+                    break;
+                case MUSEUM:
+                    markerOps.icon(BitmapDescriptorFactory.fromResource(R.drawable.logo_museum));
+                    break;
+                case CHURCH:
+                    markerOps.icon(BitmapDescriptorFactory.fromResource(R.drawable.logo_kirche));
+                    break;
+            }
+
+            Marker addedMarker = map.addMarker(markerOps);
+            markerSightMap.put(addedMarker, sight);
         }
+
+        map.setOnMarkerClickListener(this);
+        map.setOnMyLocationChangeListener(this);
+        map.setOnMapClickListener(this);
+        map.setMyLocationEnabled(true);
     }
 
     static public JSONArray loadMarkertoList(Context derKontext) {
@@ -96,5 +133,43 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             return null;
         }
         return json;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if(marker.equals(activatedMarker) && markerSightMap.containsKey(marker)) {
+            Intent startMapsActivity = new Intent(this, Detail.class);
+            AppDataExchange.currentSight = markerSightMap.get(marker);
+            startActivity(startMapsActivity);
+            return true;
+        }else{
+            activatedMarker = marker;
+            if(googleMaps.getCameraPosition().zoom < 11) {
+                googleMaps.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(marker.getPosition()).zoom(14).build()));
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public void onMyLocationChange(Location location) {
+
+        LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+
+        if(ownLocationMarker == null){
+            ownLocationMarker = googleMaps.addMarker(new MarkerOptions()
+                    .position(position)
+                    .title(getResources().getString(R.string.ownLocationMarkerText))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+            );
+            googleMaps.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(position).zoom(14).build()));
+        }else {
+            ownLocationMarker.setPosition(position);
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        activatedMarker = null;
     }
 }
--- app/src/main/java/com/mobileanwendungen/cityhunt/SplashScreen.java
+++ app/src/main/java/com/mobileanwendungen/cityhunt/SplashScreen.java
@@ -1,14 +1,14 @@
 package com.mobileanwendungen.cityhunt;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.os.Handler;
 
 import com.mobileanwendungen.cityhunt.model.SightList;
 
-public class SplashScreen extends CityHuntActivity {
+public class SplashScreen extends Activity {
 
 
     @Override
@@ -17,16 +17,15 @@ public class SplashScreen extends CityHuntActivity {
         setContentView(R.layout.activity_splash_screen);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Intent startMapsActivity = new Intent(getApplicationContext(), MapsActivity.class);
-                                    startActivity(startMapsActivity);
-
-
-                                }
-                            }, 2000
-        );
+            @Override
+            public void run() {
+                Intent startMapsActivity = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(startMapsActivity);
+                finish();
+            }
+        }, 2000);
 
         AppDataExchange.listOfSights = SightList.loadFromFile(this, "data");
     }
+
 }
